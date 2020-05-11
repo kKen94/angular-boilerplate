7e66d0e6f790d4aba457cf3008287c4630dd50ca
Nicola Taddei <nicola.taddei.94@gmail.com>
2020-05-11 16:13:59 +0200

    chore(tools): add development tools and change default project name
    • prettier and prettier-quick: code formatter
    • lint-staged: run linters on git staged files
    • commit-lint: checks if commit messages meet the conventional commit format
    • husky: allows effortless creation and management of git hooks on the client
    • webpack-bundle-analyzer: visualize size of output bundled files
    • tslint: add external and custom rules

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   }));
 
@@ -20,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'frontoffice'`, () => {
+  it(`should have as title 'frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('frontoffice');
+    expect(app.title).toEqual('frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('frontoffice app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain(
+      'frontend app is running!',
+    );
   });
 });
