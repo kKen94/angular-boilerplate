6b24b7e1ce1e3a2f6e62d5c954d07e775135aad8
Nicola Taddei <nicola.taddei.94@gmail.com>
2020-05-14 17:51:13 +0200

    feat(layuout quick-panel): add weather example

--- src/app/theme/layout/components/quick-panel/quick-panel.component.ts
+++ src/app/theme/layout/components/quick-panel/quick-panel.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-quick-panel',
   templateUrl: './quick-panel.component.html',
+  styleUrls: ['./quick-panel.component.scss'],
 })
 export class QuickPanelComponent {
   constructor() {}
