a6626c40e25234f70e82d3404b08798f74fb1f83
Nicola Taddei <nicola.taddei.94@gmail.com>
2020-06-26 11:27:51 +0200

    fix(dialog): remove - not compatible with angular 10

--- src/app/modules/pages/containers/dashboard/dashboard.module.ts
+++ src/app/modules/pages/containers/dashboard/dashboard.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { DashboardComponent } from './dashboard.component';
+import { DialogModule } from '../../../../shared/components/dialog';
 
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, DialogModule],
 })
 export class DashboardModule {}
--- src/app/shared/components/dialog/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './dialog.module';
-export * from './dialog';
-export * from './dialog-container.component';
-export * from './dialog-content.directive';
-export * from './dialog-config';
-export * from './dialog-ref';
-export * from './dialog-animations';
